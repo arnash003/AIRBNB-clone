e0592bbf45200cf7fd57a0598ad9174e8479eb19
asherjay <70672377+asherjay@users.noreply.github.com>
2021-08-13 22:53:47 +0200

    Added React Date Range Picker State to Header Component

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import {
     SearchIcon,
@@ -7,10 +7,32 @@ import {
     UseCircleIcon, 
     UsersIcon,
     UserCircleIcon,
-} from '@heroicons/react/solid'
+} from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    };
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    };
+
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
            {/*Left  */}
@@ -25,7 +47,10 @@ function Header() {
 
            {/* Middle -Search Section */}
            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-lg">
-               <input className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+               <input
+               value={searchInput}
+               onChange={(e) => setSearchInput(e.target.value)}
+               className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
                 type="text" placeholder=" Start Your Search" 
                 />
                <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" 
@@ -40,8 +65,25 @@ function Header() {
                    <UserCircleIcon className="h-6" />
                </div>
            </div>
+
+           {searchInput &&  (
+           <div className="flex flex-col col-span-3 mx-auto">
+            <DateRangePicker 
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+            />   
+            <div>
+                <h2>Number of Guests</h2>
+            </div>
+        </div>
+               )}
         </header>
     );
 }
 
-export default Header
+export default Header;
+
+// State is a short term memory for our components.You can use persistance if you need it. 
+// We create different variables inside of the component using state. 
