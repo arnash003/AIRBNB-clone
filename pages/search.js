39fe653c8f3b009aae5f5828647c003852dbace6
asherjay <70672377+asherjay@users.noreply.github.com>
2021-08-19 06:31:13 +0200

    Latest commit. need to fix startDate and endDate bug

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/dist/client/router';
+
 import React from 'react'
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
@@ -7,11 +7,7 @@ import {format} from "date-fns";
 
 function Search() {
 
-    const router = useRouter();
-    const { location, startDate, endDate, noOfGuests } = router.query;
-    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-    const formattedEndtDate = format(new Date(endDate), "dd MMMM yy");
-    const range = `${formattedStartDate} - ${formattedEndtDate}`;
+
     
 
     return (
@@ -21,9 +17,9 @@ function Search() {
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
                     <p className="text-xs">
-                        300+ stays for {noOfGuests}guests</p>
+                        300+ stays for guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">
-                        Stays in {location}
+                        Stays in 
                     </h1>
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button"> Cancellation Flexibility</p>
